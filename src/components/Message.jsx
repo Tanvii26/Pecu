571fe44c6e4cf69aa7933ffec960c67d2c423001
Tanvii26 <tanviv8745@gmail.com>
2023-01-22 13:21:13 +0530

    update ignore files.

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useRef } from 'react'
 import { AuthContext } from '../context/AuthContext'
 import { ChatContext } from '../context/ChatContext'
-import Tanvi from '../img/Tanvi.png'
 
 const Message = ({message}) => {
 
